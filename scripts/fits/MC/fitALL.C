c4213ce54dcc70e7d35cd47474544cf073f6e64c
MicheleScattola <michi.scattola@gmail.com>
2026-01-14 12:19:29 +0100

    typos

--- scripts/fits/MC/fitALL.C
+++ scripts/fits/MC/fitALL.C
@@ -71,7 +71,7 @@ void fitALL() {
                 treeName, "mu_sgn", 
                 "Muon Channel (Analytic)", 
                 "x_{#mu}",
-                0.025, 1.0  // xmin, xmax
+                0.05, 1.0  // xmin, xmax
     );
 
     // --- Electron Channel (Analytic) ---
@@ -80,7 +80,7 @@ void fitALL() {
                 treeName, "el_sgn", 
                 "Electron Channel (Analytic)", 
                 "x_{e}",
-                0.25, 1.0  // xmin, xmax
+                0.05, 1.0  // xmin, xmax
     );
 
     std::cout << ">>> All fits completed." << std::endl;
