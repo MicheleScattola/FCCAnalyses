e3da918a1a552b2da4699269e2863f5f0f454e76
Michele Scattola <michele.scattola@studenti.unimi.it>
2026-01-18 13:40:53 +0100

    new analysis brr

---
+++ scripts/analysis/MC/run_fits.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Stop on error
+set -e 
+
+echo ">>> 1. Generating templates..."
+
+root -l -b -q templatesMC.C
+
+echo ">>> 2. Fitting all decay channels..."
+
+root -l -b -q fitALL.C
+
+echo ">>> DONE."
