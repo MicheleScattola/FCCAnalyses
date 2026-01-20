f7baa465827275f46008c9a91c281afbf3fc8913
MicheleScattola <michi.scattola@gmail.com>
2026-01-20 16:11:37 +0100

    fit script

---
+++ scripts/analysis/RECO/segmented_analysis/run_fits.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Stop on error
+set -e 
+
+echo ">>> 1. Merging template files..."
+
+python3 merge.py
+
+echo ">>> 2. Creating template histograms..."
+
+root -l -b -q create_templates.C
+
+echo ">>> 3. Fitting all data files..."
+
+root -l -b -q fitALL.C
+
+echo ">>> DONE."
